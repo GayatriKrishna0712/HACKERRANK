93500e36b4156b892b86f84f76eb9e6a6551f93a
Gayatri Krishna <93417245+GayatriKrishna0712@users.noreply.github.com>
2022-10-29 09:56:16 +0530

    Create weather_observation_station_5.sql

---
+++ SQL/Easy/weather_observation_station_5.sql
@@ -0,0 +1,17 @@
+/* 
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+(SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC, City LIMIT 1) UNION (SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, City LIMIT 1);
